04501a30fe72f8726431986a4fa1e9be8519c39a
davekurk <berseker1987@hotmail.it>
2023-06-08 17:45:13 +0200

    first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use zero2prod::run;
 
-async fn healt_check() -> impl Responder {
-    HttpResponse::Ok()
-}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/healt_check", web::get().to(healt_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run()?.await
 }
